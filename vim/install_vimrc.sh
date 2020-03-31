20b391af205dc0a64008149061797959f05b235a
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-03-31 14:20:55 +0000

    use git instead of curl for obtaining the vim-plug

--- vim/install_vimrc.sh
+++ vim/install_vimrc.sh
@@ -15,7 +15,11 @@ if [ -f plug.vim ]; then
     echo "${HOME}/.vim/plug.vim exists, OVERWRITE!"
     cp plug.vim plug.vim.`date %Y%m%d%M%S`
 fi
-curl -fLo plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Some HPC's do not allow curl; use the clone instead
+#curl -fLo plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/junegunn/vim-plug.git
+cp -R vim-plug/plug.vim $HOME/.vim/autoload/
 
 # List all the plugins to be used:
 plugins="
