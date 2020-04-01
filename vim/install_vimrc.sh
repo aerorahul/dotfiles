45405e599b46954f296fa677da12bd369ce7e0fe
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-04-01 18:09:58 +0000

    add a install_bash file

--- vim/install_vimrc.sh
+++ vim/install_vimrc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh --login
 
-datestr=`date %Y%m%d%M%S`
+datestr=`date +"%Y%m%d"`
 
-[[ -f $HOME/.vimrc ]] mv $HOME/.vimrc $HOME.vimrc-$datestr
+[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME.vimrc-$datestr
 cp vimrc $HOME/.vimrc
 
 cd ${HOME}
@@ -13,7 +13,7 @@ cd ${HOME}/.vim/autoload
 # Use of plugins requires plug.vim; get the latest version
 if [ -f plug.vim ]; then
     echo "${HOME}/.vim/plug.vim exists, OVERWRITE!"
-    cp plug.vim plug.vim.`date %Y%m%d%M%S`
+    cp plug.vim plug.vim-$datestr
 fi
 
 # Some HPC's do not allow curl; use the clone instead
