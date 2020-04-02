bec2919acd525c328f259aee79f3429cf7794ac4
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-04-02 15:00:54 -0400

    more bash fixes
    Change-Id: If172a6132eb6494133a5bc0a202fb9df73df2ae0

--- bash/install_bash.sh
+++ bash/install_bash.sh
@@ -12,4 +12,15 @@ for file in $files; do
 	cp $file $HOME/.$file
 done
 
+# Grab git friendly files for bash from url below
+url="https://github.com/git/git/blob/master/contrib/completion"
+files="
+git-prompt.sh
+git-completion.bash
+"
+
+for file in $files; do
+  [[ -f $HOME/.$file ]] && mv $HOME/.$file $HOME/.$file-$datestr
+	wget $url/$file $HOME/.$file
+done
 exit 0
