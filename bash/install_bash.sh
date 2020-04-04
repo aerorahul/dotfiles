7c1860e965ced97476c4976dc3b265698a636e91
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-04-04 03:16:01 +0000

    fixes

--- bash/install_bash.sh
+++ bash/install_bash.sh
@@ -9,18 +9,17 @@ bash_functions
 "
 for file in $files; do
   [[ -f $HOME/.$file ]] && mv $HOME/.$file $HOME/.$file-$datestr
-	cp $file $HOME/.$file
+  cp $file $HOME/.$file
 done
 
 # Grab git friendly files for bash from url below
-url="https://github.com/git/git/blob/master/contrib/completion"
+url="https://raw.githubusercontent.com/git/git/master/contrib/completion"
 files="
 git-prompt.sh
 git-completion.bash
 "
-
 for file in $files; do
   [[ -f $HOME/.$file ]] && mv $HOME/.$file $HOME/.$file-$datestr
-	wget $url/$file $HOME/.$file
+  wget $url/$file $HOME/.$file
 done
 exit 0
