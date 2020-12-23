bf9024788151e9219e29d1774a406b3546733b2a
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-12-23 14:58:00 -0500

    use faster git prompts

--- bash/install_bash.sh
+++ bash/install_bash.sh
@@ -15,11 +15,14 @@ done
 # Grab git friendly files for bash from url below
 url="https://raw.githubusercontent.com/git/git/master/contrib/completion"
 files="
-git-prompt.sh
 git-completion.bash
 "
 for file in $files; do
   [[ -f $HOME/.$file ]] && mv $HOME/.$file $HOME/.$file-$datestr
   wget $url/$file && mv $file $HOME/.$file
 done
+
+# Grab git prompt for bash
+git clone https://github.com/magicmonty/bash-git-prompt $HOME/.bash-git-prompt
+
 exit 0
